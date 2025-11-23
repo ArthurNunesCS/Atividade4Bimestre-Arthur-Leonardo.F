23ea00dd49992421f8e84f286a53f712631907e0
Arthur <nunesarthur768@gmail.com>
2025-11-23 15:53:10 -0300

    7 - Finalizando o codigo

--- Atividade4Bimestre-Arthur-Leonardo.F/Form4.cs
+++ Atividade4Bimestre-Arthur-Leonardo.F/Form4.cs
@@ -54,51 +54,6 @@ namespace Atividade4Bimestre_Arthur_Leonardo.F
 
         private void btnwhile_Click(object sender, EventArgs e)
         {
-            int i, comodo;
-            double largura, comp, areacomodo, areatotal;
-            areatotal = 0;
-            areacomodo = 0;
-            comodo = Convert.ToInt32(txtcomodo.Text);
-            if (!int.TryParse(txtcomodo.Text, out comodo))
-            {
-                MessageBox.Show("Digite um número válido de cômodos!");
-                return; // sai do método para evitar erro
-            }
-            for (i = 1; i <= comodo; i++)
-            {
-                string compStr = Interaction.InputBox($"Digite o comprimento do cômodo {i}:");
-                string largStr = Interaction.InputBox($"Digite a largura do cômodo {i}:");
-                /*Mensagem de texto para o usuário*/
-
-                comp = Convert.ToDouble(compStr);
-                largura = Convert.ToDouble(largStr);
-                areacomodo = comp * largura;
-                txtareacomodo.Text = areacomodo.ToString();
-                areatotal = areatotal + areacomodo;
-                i++;      
-            }
-            txtareatotal.Text = areatotal.ToString();
-        }
-
-        private void btnvoltar_Click(object sender, EventArgs e)
-        {
-            Hide();
-            frmmenu menu = new frmmenu();
-            menu.Show();
-        }
-
-        private void btnlimpar_Click(object sender, EventArgs e)
-        {
-            txtareacomodo.Clear();
-            txtareatotal.Clear();
-            txtcomodo.Clear();
-            txtcomp.Clear();
-            txtlargura.Clear();
-            txtcomodo.Focus();
-        }
-
-        private void btnfor_Click(object sender, EventArgs e)
-        {
             int i = 1, comodo;
             double largura, comp, areacomodo, areatotal;
             areatotal = 0;
@@ -123,6 +78,49 @@ namespace Atividade4Bimestre_Arthur_Leonardo.F
                 i++;
             }
             txtareatotal.Text = areatotal.ToString();
+        }
+
+        private void btnvoltar_Click(object sender, EventArgs e)
+        {
+            Hide();
+            frmmenu menu = new frmmenu();
+            menu.Show();
+        }
+
+        private void btnlimpar_Click(object sender, EventArgs e)
+        {
+            txtareacomodo.Clear();
+            txtareatotal.Clear();
+            txtcomodo.Clear();
+            txtcomodo.Focus();
+        }
+
+        private void btnfor_Click(object sender, EventArgs e)
+        {
+            int i, comodo;
+            double largura, comp, areacomodo, areatotal;
+            areatotal = 0;
+            areacomodo = 0;
+            comodo = Convert.ToInt32(txtcomodo.Text);
+            if (!int.TryParse(txtcomodo.Text, out comodo))
+            {
+                MessageBox.Show("Digite um número válido de cômodos!");
+                return; // sai do método para evitar erro
+            }
+            for (i = 1; i <= comodo; i++)
+            {
+                string compStr = Interaction.InputBox($"Digite o comprimento do cômodo {i}:");
+                string largStr = Interaction.InputBox($"Digite a largura do cômodo {i}:");
+                /*Mensagem de texto para o usuário*/
+
+                comp = Convert.ToDouble(compStr);
+                largura = Convert.ToDouble(largStr);
+                areacomodo = comp * largura;
+                txtareacomodo.Text = areacomodo.ToString();
+                areatotal = areatotal + areacomodo;
+            }
+            txtareatotal.Text = areatotal.ToString();
+            
         }
     }
 }
